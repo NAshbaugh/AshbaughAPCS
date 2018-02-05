3369c671202c95a633bb68983923d533a5855bbb
Nathan Ashbaugh <Ashba.Nath2019@ssusd.org>
2018-02-05 07:41:29 -0800

    Sent commands 2/5

---
+++ Operators/operators.java
@@ -0,0 +1,31 @@
+class Operators {
+    public static void main(String args[]) {
+        // String name = "Nate";
+        // System.out.println(name);
+        // String firstName = "Nate";
+        // String lastName = "Ashbaugh";
+        // String fullName = firstName + " " + lastName;
+
+        // System.out.println(firstName);
+        // System.out.println("firstName");
+        // System.out.println(lastName);
+        // System.out.println("lastName");
+        // System.out.println(fullName);
+        // System.out.println("fullName");
+        // int currentYear = 2018;
+        // int yearsUntilGraduation = 1;
+        // int myGradYear = currentYear + yearsUntilGraduation;
+
+        // System.out.println(myGradYear);
+        // System.out.println("myGradYear");
+    int currentAge = 16;
+    int nextAge = currentAge + 1;
+    System.out.println(currentAge);
+    System.out.println(nextAge);
+
+    int halfCurrentAge = currentAge / 2;
+    int halfNextAge = nextAge / 2;
+    System.out.println(halfCurrentAge);
+    System.out.println(halfNextAge);
+    }
+}
