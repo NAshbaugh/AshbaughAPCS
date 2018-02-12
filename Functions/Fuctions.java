f98ea8dbc178931b974a329ee890b40cc7bf576e
NAshbaugh <nateashbaugh01@gmail.com>
2018-02-11 22:36:55 -0800

    What's a scanner?

---
+++ Functions/Fuctions.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class ValidationUtils {
+    public static void main(String args[]) {
+        System.out.print("Enter a string: ");
+        Scanner scan = new Scanner(System.in);
+        if(isValidString(scan)) {
+            System.out.println("String Success!");
+        }
+        else {
+            System.out.println("String Failure!");
+        }
+    }
+
+    
+    public static Boolean isValidString(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+        }
+        catch(Exception e) {
+            return false; //An error was raised. The user input could not be read as a String.
+        }
+        return true; //No error was raised. The scanner read it successfully as a String.
+    }
+
+    public static void main(int args[]) {
+        System.out.print("Enter an integer: ");
+        Scanner scan = new Scanner(System.in);
+        if(isValidInt(scan)) {
+            System.out.println("Int Success!");
+        }
+        else {
+            System.out.println("Int Failure!");
+        }
+    }
+    
+    public static Boolean isValidInt(Scanner scan) {
+        try {
+            String s = scan.nextLine();
+        }
+        catch(Exception e) {
+            return false;
+        }
+        return true; 
+    }
+}
+
