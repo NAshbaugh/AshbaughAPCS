54665d03e310ff5753eeddf0571a258290322567
NAshbaugh <nateashbaugh01@gmail.com>
2018-02-16 17:01:48 -0800

    Loan Calc Finished!

--- Projects/LoanCalculator/LoanCalculator.java
+++ Projects/LoanCalculator/LoanCalculator.java
@@ -27,10 +27,10 @@ public class LoanCalculator{
         }
         else {
             if(mode == 2) {
-                compoundingInterestNP();
+                compoundingInterestNoPayment();
             }
             else {
-                compoundingInterestWP();
+                compoundingInterestWithPayement();
                 
             }     
         }
@@ -78,7 +78,7 @@ public class LoanCalculator{
         System.out.println("Total Loan Amount: $" + loanTotal);
     }
 
-    public static void compoundingInterestNP() {
+    public static void compoundingInterestNoPayment() {
         Scanner scan;
         while(true) {
             System.out.println("How much is your loan? ($):");
@@ -111,31 +111,64 @@ public class LoanCalculator{
             }
         }
         double interestPercent = interestRate / 100;
-        double ratePerMonth = interestPercent / 12;
-        int totalMonths = loanTerm;
-        while(true) {
-            int monthsRemaining = totalMonths - 1;
-            double loanTotal =
-            while(monthsRemaining > 0) {
-                double monthlyInterest = loanTotal * ratePerMonth;
-                double trueLoanTotal = loanTotal + monthlyInterest;
-            }
-            break;
-        if(monthsRemaining <= 0) {
-            break;
-            }
-        }
-        
-        double totalInterest = trueLoanTotal - loanAmt;
+        double loanMonths = loanTerm * 12;
+        double loanTotal = loanAmt * Math.pow(1 + (interestPercent / 12), loanMonths);
+        double totalInterest = loanTotal - loanAmt;
         System.out.println("Original Loan Amount: $" + loanAmt);
         System.out.println("Loan Term: " + loanTerm + " years");
         System.out.println("Interest Rate: " + interestRate + "%");
         System.out.println("Accumulated Interest: $" + totalInterest);
-        System.out.println("Total Loan Amount: $" + trueLoanTotal);
+        System.out.println("Total Loan Amount: $" + loanTotal);
     }
 
-
-
+    public static void compoundingInterestWithPayement() {
+        Scanner scan;
+        while(true) {
+            System.out.println("How much is your loan? ($):");
+            scan = new Scanner(System.in);
+            if(scan.hasNextDouble()) {
+                loanAmt = scan.nextDouble();
+                if(0 < loanAmt) {
+                    break;
+                }
+            }
+        }
+        while(true) {
+            System.out.println("How long is your loan period? (years):");
+            scan = new Scanner(System.in);
+            if(scan.hasNextInt()) {
+                loanTerm = scan.nextInt();
+                if(0 < loanTerm) {
+                    break;
+                }
+            }
+        }
+        while(true) {
+            System.out.println("What is your given interest rate? (ex. 3.5% = 3.5):");
+            scan = new Scanner(System.in);
+            if(scan.hasNextDouble()) {
+                interestRate = scan.nextDouble();
+                if(0 < interestRate) {
+                    break;
+                }
+            }
+        }
+        double interestPercent = interestRate / 100;
+        double loanMonths = loanTerm * 12;
+        double ratePerMonth = interestPercent / 12;
+        double monthlyPayment = 1 + loanAmt * (ratePerMonth / (1 - Math.pow((1 + ratePerMonth), (loanTerm * -12))));
+        double loanTotal = loanAmt * Math.pow(1 + (interestPercent / 12), loanMonths) - monthlyPayment;
+        double remainingPayment = loanTotal % monthlyPayment;
+        double totalInterest = loanTotal - loanAmt;
+        System.out.println("Original Loan Amount: $" + loanAmt);
+        System.out.println("Loan Term: " + loanTerm + " years");
+        System.out.println("Interest Rate: " + interestRate + "%");
+        System.out.println("Minimum Monthly Payment: $" + monthlyPayment);
+        System.out.println("Last Monthly Payment: $" + remainingPayment);
+        System.out.println("Accumulated Interest: $" + totalInterest);
+        System.out.println("Total Loan Amount: $" + loanTotal);
+    }
+    
     public static void printModeStatement() {
         System.out.print(
             "Select an interest calculation mode:\n"
