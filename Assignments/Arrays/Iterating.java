807cb1d3be31226735a8df8c9f76e75f338a6324
Nathan Ashbaugh <Ashba.Nath2019@ssusd.org>
2018-04-04 08:05:59 -0700

    Added string

--- Assignments/Arrays/Iterating.java
+++ Assignments/Arrays/Iterating.java
@@ -4,7 +4,7 @@ class Iterating {
     public static void main(String[] args) {
     int[] nums = new int[201];
         for(int i = 0; i < nums.length ; i++) {
-            nums = nums * 100;
+            int newNums = nums
             System.out.println(nums);
 
         }
