d24785c1f5834675b6024bb3e839059bd9c4ba8f
Nathan Ashbaugh <Ashba.Nath2019@ssusd.org>
2018-04-05 07:38:30 -0700

    String worked on

--- Assignments/StringDecomposition/stringDecomposition.java
+++ Assignments/StringDecomposition/stringDecomposition.java
@@ -1,29 +1,39 @@
-//1
+class stringDecomposition {
+    public static void main(String[] args) {
     // String s = "I'd go, but I'm broke.";
 
     // char c1 = s.charAt(7);
     // System.out.println(c1);
-
+    //     " "
     // char c2 = s.charAt(10);
     // System.out.println(c);
-
+    //     "t"
     // int idx1 = s.indexOf(',');
     // System.out.println(idx1);
-
+    //     "6"
     // int idx2 = idx1 + 3;
     // System.out.println(idx2);
-
+    //     "9"
     // char c3 = s.charAt(idx1);
     // System.out.println(c3);
-
+    //     "  '  "
     // char c4 = s.charAt(s.indexOf(c2) + 2);
     // System.out.println(c4);
-
+    //     "I"
     // char c5 = s.charAt(s.length() - 1);
     // System.out.println(c5);
-
+    //     "."
     // int idx3 = s.indexOf('\'');
     // System.out.println(idx3);
-
+    //     "1"
     // int idx4 = s.indexOf("go");
     // System.out.println(idx4);
+    //     "4"
+
+    String s = "Given the circumstances, I'm not sure if it was a success, but it certainly seems promising."
+        new ArrayList<Integer> positions = -1;
+        for( int i = 0; i < s.length(); i++ ) {
+            
+        }
+    }
+}
